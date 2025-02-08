af8ebac52b3d8a11b0b1ac43399c26381a395a51
하현진 <mnbdj12345@gmail.com>
2025-02-08 13:34:09 +0900

    Update: run_steam_deep_fm.sh
    how to:
    sh run_steam_deep_fm.sh

--- run_steam_deep_fm.sh
+++ run_steam_deep_fm.sh
@@ -31,20 +31,3 @@ wait
 # Dataset: steam Backbone: deepfm Warmup_model: CVAR
 nohup python main.py --dataset_name steam --model_name deepfm --warmup_model cvar --cvar_iters 10 --pretrain_model_path pretrain_backbones > nohup_steam_cvar.log 2>&1 &
 wait
-
-'''## steam_500_15
-
-nohup python main.py --dataset_name steam_500_15 --model_name deepfm --warmup_model base --pretrain_model_path pretrain_backbones > nohup_steam_500_15_base.log 2>&1 &
-wait  # 해당 프로세스가 끝날 때까지 대기
-
-nohup python main.py --dataset_name steam_500_15 --model_name deepfm --warmup_model metaE --pretrain_model_path pretrain_backbones > nohup_steam_500_15_metaE.log 2>&1 &
-wait  
-
-nohup python main.py --dataset_name steam_500_15 --model_name deepfm --warmup_model mwuf --pretrain_model_path pretrain_backbones > nohup_steam_500_15_mwuf.log 2>&1 &
-wait  
-
-#nohup python main.py --dataset_name steam_500_15 --model_name deepfm --warmup_model cvar_init --cvar_iters 10 --pretrain_model_path pretrain_backbones > nohup_steam_cvar_init.log 2>&1 &
-#wait  
-
-nohup python main.py --dataset_name steam_500_15 --model_name deepfm --warmup_model cvar --cvar_iters 10 --pretrain_model_path pretrain_backbones > nohup_steam_500_15_cvar.log 2>&1 &
-wait  '''
